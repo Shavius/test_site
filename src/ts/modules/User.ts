f56d8448ff8b2931a7e74b0e5065c0429f177091
Shavius <glamurparty@ukr.net>
2024-10-31 17:59:54 +0200

    Update Class User add race

--- src/ts/modules/User.ts
+++ src/ts/modules/User.ts
@@ -1,13 +1,15 @@
 export default class User {
 	protected name: string;
 	protected age: number;
+	protected race: string;
 
 	constructor(userName: string, userAge: number) {
 		this.name = userName;
 		this.age = userAge;
+		this.race = "Human";
 	}
 
 	public getInfo(): string {
-		return `Name: ${this.name} Age: ${this.age}`;
+		return `Race: ${this.race} Name: ${this.name} Age: ${this.age}`;
 	}
 }
