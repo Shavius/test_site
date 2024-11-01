bad8bca44fd69e261db1aa4a57e641b3eebff47d
Tolgran <tolgran@ukr.net>
2024-11-01 16:52:09 +0200

    Update Wizard.spec.ts and 4 tests

--- src/ts/specs/Wizard.spec.ts
+++ src/ts/specs/Wizard.spec.ts
@@ -2,7 +2,22 @@ import Wizard from "../modules/Wizard";
 
 describe("Wizard", () => {
 	it("getInfo", () => {
-		const wizard = new Wizard("Wizard", 20);
-		expect(wizard.getInfo()).toBe("Race: Human Name: Wizard Age: 20 - Class: Wizard");
+		const wizard = new Wizard("Wizard 1", 20);
+		expect(wizard.getInfo()).toBe("Race: Human Name: Wizard 1 Age: 20 - Class: Wizard");
+	});
+
+	it("getInfo", () => {
+		const wizard = new Wizard("Wizard 2", 25);
+		expect(wizard.getInfo()).toBe("Race: Human Name: Wizard 2 Age: 25 - Class: Wizard");
+	});
+
+	it("getInfo", () => {
+		const wizard = new Wizard("Wizard 3", 30);
+		expect(wizard.getInfo()).toBe("Race: Human Name: Wizard 3 Age: 30 - Class: Wizard");
+	});
+
+	it("getInfo", () => {
+		const wizard = new Wizard("Wizard 4", 35);
+		expect(wizard.getInfo()).toBe("Race: Human Name: Wizard 4 Age: 35 - Class: Wizard");
 	});
 });
