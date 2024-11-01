ca4dc94d459ad33a1f8f1d7ee282e965c4d4fb81
Tolgran <tolgran@ukr.net>
2024-11-01 16:57:58 +0200

    Fix Wizard.spec.ts change names

--- src/ts/specs/Wizard.spec.ts
+++ src/ts/specs/Wizard.spec.ts
@@ -1,22 +1,22 @@
 import Wizard from "../modules/Wizard";
 
 describe("Wizard", () => {
-	it("getInfo", () => {
+	it("getInfo Wizard 1", () => {
 		const wizard = new Wizard("Wizard 1", 20);
 		expect(wizard.getInfo()).toBe("Race: Human Name: Wizard 1 Age: 20 - Class: Wizard");
 	});
 
-	it("getInfo", () => {
+	it("getInfo Wizard 2", () => {
 		const wizard = new Wizard("Wizard 2", 25);
 		expect(wizard.getInfo()).toBe("Race: Human Name: Wizard 2 Age: 25 - Class: Wizard");
 	});
 
-	it("getInfo", () => {
+	it("getInfo Wizard 3", () => {
 		const wizard = new Wizard("Wizard 3", 30);
 		expect(wizard.getInfo()).toBe("Race: Human Name: Wizard 3 Age: 30 - Class: Wizard");
 	});
 
-	it("getInfo", () => {
+	it("getInfo Wizard 4", () => {
 		const wizard = new Wizard("Wizard 4", 35);
 		expect(wizard.getInfo()).toBe("Race: Human Name: Wizard 4 Age: 35 - Class: Wizard");
 	});
